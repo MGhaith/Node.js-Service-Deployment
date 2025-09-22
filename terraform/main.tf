0e13b04788c0c7e0b97ef04b38cd5e5d7fa54c01
Ghaith Magherbi <retr0creativegm@gmail.com>
2025-09-22 23:37:33 +0100

    Add IaC to provision EC2 Ubuntu server

---
+++ terraform/main.tf
@@ -0,0 +1,44 @@
+resource "aws_key_pair" "default" {
+  key_name   = "node-app-key"
+  public_key = file("~/.ssh/id_rsa.pub")
+}
+
+resource "aws_instance" "server" {
+  ami           = "ami-0360c520857e3138f" # Ubuntu 24.04
+  instance_type = "t3.micro"
+  key_name      = aws_key_pair.default.key_name
+
+  tags = {
+    Name = "node-app-server"
+  }
+
+  security_groups = [aws_security_group.allow_http_ssh.name]
+}
+
+resource "aws_security_group" "allow_http_ssh" {
+  name        = "allow_http_ssh"
+  description = "Allow SSH and HTTP inbound traffic"
+
+  ingress {
+    description = "SSH"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description = "HTTP"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
