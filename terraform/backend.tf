adab3eee8f41e0d6e78557590d32e038713a8a69
Ghaith Magherbi <retr0creativegm@gmail.com>
2025-09-30 05:27:29 +0100

    Update backend.tf
    clean terraform backend values

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -2,10 +2,10 @@ terraform {
   required_version = ">= 1.13.0"
 
   backend "s3" {
-    bucket         = "node-app-terraform-state-075091538636"        # S3 bucket name (create once manually)
-    key            = "global/terraform.tfstate"                     # Path inside the bucket
-    region         = "us-east-1"                                    # Best Region for you
-    dynamodb_table = "node-app-terraform-locks"                     # DynamoDB table for state locking
-    encrypt        = true                                           # Encrypt state at rest
+    bucket         = "node-app-terraform-state-<Your AWS Account ID>"       # S3 bucket name (create once manually)
+    key            = "global/terraform.tfstate"                             # Path inside the bucket
+    region         = "us-east-1"                                            # Best Region for you
+    dynamodb_table = "node-app-terraform-locks"                             # DynamoDB table for state locking
+    encrypt        = true                                                   # Encrypt state at rest
   }
 }
